546ab1d546aa2682cafc6cc73b6a5c26c0d28b3e
LawEyez <allenojuka10@gmail.com>
2024-09-24 13:07:42 +0300

    Add data.

---
+++ src/data.ts
@@ -0,0 +1,45 @@
+export const activities = [
+  {
+    name: 'Netball',
+    description: `Our extracurriculars foster well-rounded students by encouraging growth, leadership, and teamwork through sports, arts, and clubs.`
+  },
+  {
+    name: 'Handball',
+    description: `Our extracurriculars foster well-rounded students by encouraging growth, leadership, and teamwork through sports, arts, and clubs.`
+  },
+  {
+    name: 'Football',
+    description: `Our extracurriculars foster well-rounded students by encouraging growth, leadership, and teamwork through sports, arts, and clubs.`
+  },
+  {
+    name: 'Volleyball',
+    description: `Our extracurriculars foster well-rounded students by encouraging growth, leadership, and teamwork through sports, arts, and clubs.`
+  },
+  {
+    name: 'Athletics',
+    description: `Our extracurriculars foster well-rounded students by encouraging growth, leadership, and teamwork through sports, arts, and clubs.`
+  },
+  {
+    name: 'Music',
+    description: `Our extracurriculars foster well-rounded students by encouraging growth, leadership, and teamwork through sports, arts, and clubs.`
+  },
+]
+
+export const events = [
+  {
+    title: 'School cultural day',
+    date: '22 September 2024',
+  },
+  {
+    title: 'School cultural day',
+    date: '22 September 2024',
+  },
+  {
+    title: 'School cultural day',
+    date: '22 September 2024',
+  },
+  {
+    title: 'School cultural day',
+    date: '22 September 2024',
+  },
+]
