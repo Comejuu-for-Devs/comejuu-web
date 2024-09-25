5402a055724b833f2c53da33637a9b7963475a0d
LawEyez <allenojuka10@gmail.com>
2024-09-25 10:38:03 +0300

    Add prop types.

--- src/components/home/NewsCard.tsx
+++ src/components/home/NewsCard.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image'
 import React from 'react'
 
-export default function NewsCard({ title, content }) {
+type NewsCardProps = {
+  title: string
+  content: string
+}
+
+const NewsCard = ({ title, content }: NewsCardProps) => {
   return (
     <div className='flex gap-4 w-full'>
       <Image
@@ -9,7 +14,8 @@ export default function NewsCard({ title, content }) {
         height={1280}
         src='https://images.unsplash.com/photo-1460518451285-97b6aa326961?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         alt=''
-        className='w-[150px] h-[120px] object-cover rounded-2xl' />
+        className='w-[150px] h-[120px] object-cover rounded-2xl'
+      />
       <div className='w-[426px] space-y-4'>
         <p className='text-black font-bold text-xl'>{title}</p>
         <p className='text-xs text-neutral-dark'>{content}</p>
@@ -17,4 +23,6 @@ export default function NewsCard({ title, content }) {
     </div>
   )
 }
+
+export default NewsCard
 
