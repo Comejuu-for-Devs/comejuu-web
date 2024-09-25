4c380bc95a2d5bf961d4c56258cb7767eb9a06ca
MingiJ <jeffawesome.ja@gmail.com>
2024-09-25 11:27:19 +0300

    Create the hero section of the admissions page

--- src/app/admissions/page.tsx
+++ src/app/admissions/page.tsx
@@ -3,37 +3,52 @@ import React from 'react'
 
 const Admissions = () => {
   return (
-    <section className='page min-h-screen'>
+    <section className=' min-h-screen'>
       <div className="space-y-20">
-        <div className='mt-20'>
-          <div className='flex gap-5 p-5 absolute top-20 w-full'>
-            <Image
-              width={1920}
-              height={1280}
-              src='https://images.unsplash.com/photo-1460518451285-97b6aa326961?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-              alt=''
-              className='w-full h-[400px] object-cover'
-            />
-            <Image
-              width={1920}
-              height={1280}
-              src='https://images.unsplash.com/photo-1460518451285-97b6aa326961?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-              alt=''
-              className='w-full h-[300px] object-cover'
-            />
-            <Image
-              width={1920}
-              height={1280}
-              src='https://images.unsplash.com/photo-1460518451285-97b6aa326961?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-              alt=''
-              className='w-full h-[300px] object-cover'
-            />
+        <div className="relative w-full bg-black">
+          <div className='relative'>
+            <div className="absolute inset-0 bg-black bg-opacity-50 z-20"></div>
+            <div className="flex gap-5 p-5 relative z-10">
+              <Image
+                width={1920}
+                height={1280}
+                src="https://images.unsplash.com/photo-1460518451285-97b6aa326961?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+                className="w-full h-[600px] object-cover"
+              />
+              <Image
+                width={1920}
+                height={1280}
+                src="https://images.unsplash.com/photo-1460518451285-97b6aa326961?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+                className="w-full h-[600px] object-cover"
+              />
+              <Image
+                width={1920}
+                height={1280}
+                src="https://images.unsplash.com/photo-1460518451285-97b6aa326961?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+                className="w-full h-[600px] object-cover"
+              />
+            </div>
+            <h1 className="absolute inset-0 flex items-center justify-center text-white text-8xl lg:text-[240px] font-light z-30">
+              Comejuu
+            </h1>
+          </div>
+          <div className='text-white mx-auto max-w-screen-xl flex gap-10 p-5'>
+            <p className='max-w-prose text-lg'>
+              We know choosing the right international school in Kenya is a major decision. It goes beyond looking at a school's academic reputation. It is also important to consider the social and extracurricular opportunities available to every student. This will enable you to see what an ISK experience means for your whole family.
+            </p>
+            <div>
+              <p className='text-tertiary max-w-32'>Applications Now Available</p>
+            </div>
+            <div>
+              <p className='text-tertiary'>What's next?</p>
+            </div>
 
           </div>
-          <h1 className="font-secondary text-center text-4xl sm:text-[240px] font-light">
-            Comejuu
-          </h1>
         </div>
+
       </div>
     </section>
   )
