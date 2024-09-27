dc6c0ed15772f22ea277021328b991d2e82d48b0
LawEyez <allenojuka10@gmail.com>
2024-09-27 13:37:23 +0300

    Stage updates.

--- src/components/home/StageCard.tsx
+++ src/components/home/StageCard.tsx
@@ -4,17 +4,24 @@ type StageCardProps = {
   name: string
   bgStyles: string
   textStyles: string
+  level: string
 }
 
-const StageCard: React.FC<StageCardProps> = ({ name, bgStyles, textStyles }) => {
+const StageCard: React.FC<StageCardProps> = ({
+  name,
+  bgStyles,
+  textStyles,
+  level,
+}) => {
   const [first, second] = name.split(' ')
 
   return (
-    <div className={`flex flex-col justify-between p-5 rounded-xl border border-gray-100
-    h-[250px] ${bgStyles}`}>
-      <div className="w-20 h-20 rounded-lg bg-gray-300"></div>
+    <div className={`flex flex-col justify-between p-5 border-2 border-transparent
+    h-[300px] sm:h-[400px] ${bgStyles} hover:scale-95 transition cursor-pointer`}>
+      {/* <div className="w-20 h-20 rounded-lg bg-gray-300"></div> */}
+      <p className={`text-sm uppercase ${textStyles}`}>{level}</p>
 
-      <h3 className={`text-2xl font-secondary flex flex-col ${textStyles}`}>
+      <h3 className={`text-2xl sm:text-4xl font-secondary flex flex-col ${textStyles}`}>
         <span className="font-bold">{first}</span>
         <span className="font-light">{second}</span>
       </h3>
--- src/components/home/Stages.tsx
+++ src/components/home/Stages.tsx
@@ -3,27 +3,31 @@ import StageCard from './StageCard'
 
 const Stages = () => {
   return (
-    <section className='py-20 bg-gray-50'>
-      <div className="container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2">
+    <section className='py-20'>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
         <StageCard
           name='Pre school'
           bgStyles='bg-primary'
           textStyles='text-white'
+          level='playgroup & kindergarten'
         />
         <StageCard
           name='Lower school'
           bgStyles='bg-secondary'
           textStyles='text-white'
+          level='grade 1 - 3'
         />
         <StageCard
           name='Middle school'
           bgStyles='bg-tertiary'
           textStyles='text-primary'
+          level='grade 4 - 6'
         />
         <StageCard
           name='Junior secondary'
-          bgStyles='bg-white'
+          bgStyles='bg-gray-100'
           textStyles='text-primary'
+          level='grade 7 - 9'
         />
       </div>
     </section>
