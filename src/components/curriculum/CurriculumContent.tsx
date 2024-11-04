9de7b6497139a99a3ba57d2fdec0b326513d04dd
Louis Muriuki <luihugo247@gmail.com>
2024-11-04 18:12:37 +0300

    curriullum page fix

--- src/components/curriculum/CurriculumContent.tsx
+++ src/components/curriculum/CurriculumContent.tsx
@@ -11,12 +11,11 @@ import Dots from "@/components/layout/Dots";
 import CallToAction from "@/components/CallToAction";
 import CurriculumExcellenceTabs from "@/components/curriculum/CurriculumTabs";
 import ApproachTabs from "@/components/curriculum/ApproachTabs";
-
-import ApplyNowButton from "@/components/ApplyNowButton";
+import AgricultureHarvest from "../../../public/images/curriculum/agriculture_harvest.jpg";
+import HeroSection from "../HeroSection";
 
 const CurriculumContent = () => {
   const searchParams = useSearchParams();
-  const router = useRouter();
   useEffect(() => {
     const section = searchParams?.get("section");
 
@@ -48,29 +47,12 @@ const CurriculumContent = () => {
 
       <div className="space-y-10 sm:space-y-20">
         {/* HERO */}
-        <div className="relative w-full h-[60vh]">
-          <Image
-            width={1920}
-            height={1280}
-            src="https://images.unsplash.com/photo-1460518451285-97b6aa326961?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt=""
-            className="asbolute w-full h-full object-cover rounded-xl"
-          />
-
-          <div className="absolute top-0 bg-black/80 h-full w-full z-10">
-            <div className="container py-20 h-full flex flex-col gap-10 justify-center">
-              <h1 className="font-secondary text-4xl w-full  sm:text-6xl font-black tracking-tight text-white">
-                Where Your Child&apos;s Future Takes Flight With Confidence
-              </h1>
-
-              <ApplyNowButton
-                onClick={() => {
-                  router.push("/admissions");
-                }}
-              />
-            </div>
-          </div>
-        </div>
+        <HeroSection
+          img={AgricultureHarvest}
+          title="Our Curriculum empowers your child to soar like an eagle towards a bright future"
+          buttonText="Fill Admission Form"
+          buttonLink="/admissions"
+        />
 
         {/* APPROACH */}
         <div className="container space-y-10">
@@ -85,33 +67,6 @@ const CurriculumContent = () => {
             <p className="text-lg sm:text-xl font-light">
               Commitment to Excellence is in our DNA
             </p>
-          </div>
-
-          {/* IMAGE CAROUSEL */}
-          <div className="overflow-x-auto pb-5">
-            <div className="flex gap-2 w-max">
-              <Image
-                width={1920}
-                height={1280}
-                src="https://images.unsplash.com/photo-1460518451285-97b6aa326961?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt=""
-                className="h-[300px] w-[500px] object-cover rounded-lg"
-              />
-              <Image
-                width={1920}
-                height={1280}
-                src="https://images.unsplash.com/photo-1460518451285-97b6aa326961?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt=""
-                className="h-[300px] w-[500px] object-cover rounded-lg"
-              />
-              <Image
-                width={1920}
-                height={1280}
-                src="https://images.unsplash.com/photo-1460518451285-97b6aa326961?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt=""
-                className="h-[300px] w-[500px] object-cover rounded-lg"
-              />
-            </div>
           </div>
 
           <ApproachTabs />
